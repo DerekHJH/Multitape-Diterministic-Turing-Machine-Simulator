d7b300994e6fa8ac0143f6de72d6fd1f5a0615d7
181240020-Hu Junhao <1185276716@qq.com>
2020-12-06 15:00:50 +0800

    finish processing eg.tm file

--- turing.cpp
+++ turing.cpp
@@ -31,25 +31,58 @@ class _turing
 
 #define MAX_IN_CHAR 128
 		int numInChar;
-		char *inChar[MAX_IN_CHAR];
+		char inChar[MAX_IN_CHAR];
 
 #define MAX_TAPE_CHAR 256
 		int numTapeChar;
-		char *tapeChar[MAX_TAPE_CHAR];
+		char tapeChar[MAX_TAPE_CHAR];
 
 #define MAX_NUM_ACCEPT 256
 		int numAccept;
 		char *accept[MAX_NUM_ACCEPT];
 
-		char q0, B;
+#define MAX_LEN_STATE 256
+		char q0[MAX_LEN_STATE];
+		char B;
+
+#define MAX_NUM_TRANS 1024
+		int numTrans;
+		char *trans[MAX_NUM_TRANS][5];
 
 #define TAPE_MIDDLE 10000
+		int numTape = 0;
 		char tape[64][2 * TAPE_MIDDLE];
 		int head[64];
-		int numTape = 0;
+
+		void extractToken(int *tot, char *ss[], char *s, char *buf, bool isString)
+    {
+			(*tot) = 0;
+			int l = strlen(buf), p = 0;
+			char *c = NULL;
+			while(p < l && buf[p] != '{')p++;
+			while(l >= 0 && buf[l] != '}')l--;
+			buf[l] = '\0';
+			c = strtok(&buf[p + 1], ",");
+			while(c != NULL)
+			{
+				(*tot)++;
+				if(isString)
+				{
+					ss[(*tot)] = (char *)malloc(strlen(c) + 1);
+					ss[(*tot)][0] = '\0';
+					strcpy(ss[(*tot)], c);
+				}
+				else
+				{
+					s[(*tot)] = c[0];
+				}
+				c = strtok(NULL, ",");
+			}
+		}
 
 		_turing()
 		{
+			numTrans = 0;
 			FILE *fp = fopen(turingfile, "r");
 			if(fp == NULL)
 			{
@@ -61,48 +94,42 @@ class _turing
 				fscanf(fp, "%[^\n]\n", buf);
 				if(buf[0] == '#')
 				{
-					if(buf[1] == 'Q')extractTokens(&numState, state, &buf[0]);
-					else if(buf[1] == 'S')extractTokens(&numInChar, inChar, &buf[0]);
-					else if(buf[1] == 'G')extractTokens(&numTapeChar, tapeChar, &buf[0]);
-					else if(buf[1] == 'F')extractTokens(&numAccept, accept, &buf[0]);
-					else if(buf[1] == 'N')
-					{
-						int l = strlen(buf), p = 0;
-            while(p < l && (buf[p] < '0' || buf[p] > '9'))p++;
-						numTape = atoi((const char*)(&buf[p]));
-					}
+					if(buf[1] == 'Q')extractToken(&numState, state, NULL, buf, true);
+					else if(buf[1] == 'S')extractToken(&numInChar, NULL, inChar, buf, false);
+					else if(buf[1] == 'G')extractToken(&numTapeChar, NULL, tapeChar, buf, false);
+					else if(buf[1] == 'q')q0[0] = '\0', strcpy(q0, &buf[6]);
+					else if(buf[1] == 'B')B = buf[5];
+					else if(buf[1] == 'F')extractToken(&numAccept, accept, NULL, buf, true);
+					else if(buf[1] == 'N')numTape = atoi((const char*)(&buf[5]));
 				}
 				else if((buf[0] >= 'a' && buf[0] <= 'z') || (buf[0] >= 'A' && buf[0] <= 'Z') || (buf[0] >= '0' && buf[0] <= '9') || buf[0] == '_')
 				{
+					numTrans++;
+				  char *c = strtok(buf, " ");
+					int cnt = -1;
+          while(c != NULL)
+          {
+						cnt++;
+						trans[numTrans][cnt] = (char *)malloc(strlen(c) + 1);
+          	trans[numTrans][cnt][0] = '\0';
+          	strcpy(trans[numTrans][cnt], c);
+          	c = strtok(NULL, " ");
+          }
 				}
 			}
 			fclose(fp);
 		}	
-
-
-		void extractTokens(int *tot, char *s[], char *buf)
-    {
-    	(*tot) = 0;
-      int l = strlen(buf), p = 0;
-      char *c = NULL;
-      while(p < l && buf[p] != '{')p++;
-      while(l >= 0 && buf[l] != '}')l--;
-      buf[l] = '\0';
-      c = strtok(&buf[p + 1], ",");
-      while(c != NULL)
-      {
-      	(*tot)++;
-    		s[(*tot)] = (char *)malloc(strlen(c) + 1);
-      	s[(*tot)][0] = '\0';
-      	strcpy(s[(*tot)], c);
-      	c = strtok(NULL, ",");
-      }
-    }
-
 		~_turing()
 		{
-			//free memory previously allocted in _turing()
+			for(int i = 1; i <= numState; i++)
+				free(state[i]);
+			for(int i = 1; i <= numAccept; i++)
+				free(accept[i]);
+			for(int i = 1; i <= numTrans; i++)
+      	for(int j = 0; j <= 4; j++)
+      		free(trans[i][j]);
 		}
+		
 
 };
 
@@ -136,18 +163,25 @@ int main(int argc, char *argv[])
 	printf("\n\n");
 	
 	for(int i = 1; i <= TM.numInChar; i++)
-		printf("%s ", TM.inChar[i]);
+		printf("%c ", TM.inChar[i]);
 	printf("\n\n");
 	
 	for(int i = 1; i <= TM.numTapeChar; i++)
-		printf("%s ", TM.tapeChar[i]);
+		printf("%c ", TM.tapeChar[i]);
 	printf("\n\n");
 	
 	for(int i = 1; i <= TM.numAccept; i++)
   	printf("%s ", TM.accept[i]);
   printf("\n\n");
 	
-	printf("%d", TM.numTape);
+	printf("%d %s %c", TM.numTape, TM.q0, TM.B);
 	printf("\n\n");
+
+	for(int i = 1; i <= TM.numTrans; i++)
+	{
+		for(int j = 0; j <= 4; j++)
+			printf("%s ", TM.trans[i][j]);
+		printf("\n");
+	}
 	return 0;
 }
